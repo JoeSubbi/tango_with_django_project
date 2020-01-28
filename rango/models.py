42afa625b60634c1929b803bad41efc5fafd14ad
JoeSubbi <joe.subbiani@gmail.com>
2020-01-28 20:02:56 +0000

    5-5.4 Creating and Migrating the Database

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True) #< every name is unique. can be used as pk
+
+    def __str__(self):#< equivilant of toString() in java
+        return self.name
+
+
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)#< fk one to many relationships
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
